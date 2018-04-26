502887e82acb2c76829a7e6c4cb5433ecf8fe99b
André Ferreira <contato@andreferreira.me>
2018-04-25 23:49:51 -0300

    Lista 01 - Exercício 01 e 02

---
+++ lista-01/exercicio-01.py
@@ -0,0 +1,4 @@
+a = int(input('Digite um número inteiro: '))
+b = int(input('Digite um número inteiro: '))
+
+print('A soma dos números é', a + b)
---
+++ lista-01/exercicio-02.py
@@ -0,0 +1,3 @@
+metros = float(input('Digite um valor em metros: '))
+
+print('O valor em mílímetros é', metros * 1000, 'milímetros')
